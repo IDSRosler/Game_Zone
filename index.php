68439cc9f90677cfc150d85aa1d93a259c690db1
IDSRosler <igorsouzarosler@gmail.com>
2019-11-18 09:14:06 -0300

    Estrutura de arquivos e index.php pronto

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>GameZone</title>
+	<link href="https://fonts.googleapis.com/css?family=Press+Start+2P&display=swap" rel="stylesheet"> 
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<dir id="principal">
+
+		<section id="cabecalho">
+			<?php include_once("cabecalho.php"); ?>
+		</section> <!--	fim div cabecalho -->
+
+		<section id="corpo">
+			<?php include_once("home.php"); ?>
+		</section> <!--	fim div corpo -->
+
+		<footer id="rodape">
+			<?php include_once("rodape.php"); ?>
+		</footer> <!--	fim div rodape -->
+
+	</dir> <!--	fim div principal -->
+</body>
+</html>
