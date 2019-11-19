f7d28ae7adc056320703531f33174cb7155aa376
IDSRosler <igorsouzarosler@gmail.com>
2019-11-19 00:17:10 -0300

    add: Página de detalhes e arquivos javascript

---
+++ GameZone/home.php
@@ -0,0 +1,91 @@
+<div id="corpo_loja">
+	<aside class="banner">
+		<img src="imagens/banner.jpg" width="100%">
+	</aside>
+	<section class="categorias">
+		<h2 class="fundo_cinza"> Plataformas </h2>
+		<nav>
+			<ul>
+				<li><input type="checkbox" name="playstation"> PlayStation </li>
+				<li><input type="checkbox" name="xbox"> XBox </li>
+				<li><input type="checkbox" name="computador"> Computador </li>
+			</ul>
+		</nav>	
+		<h2 class="fundo_cinza"> Categorias </h2>
+		<nav>
+			<ul>
+				<li><input type="checkbox" name="acao"> Ação </li>
+				<li><input type="checkbox" name="aventura"> Aventura </li>
+				<li><input type="checkbox" name="sporte"> Esportes </li>
+			</ul>
+		</nav>	
+	</section>
+	<div id="lado_direito">
+		<h3 class="titulo fundo_cinza"> Lista de jogos </h3>
+		<section class="vitrine">
+			<ul>
+				<li>
+					<a href="#">
+						<figure>
+							<img src="imagens/game.jpg" alt="Uncharted 4">
+							<figcaption>Uncharted 4</figcaption>
+						</figure>
+						<span> RS 100,00 </span>
+						<form action="">
+							<input type="submit" value="">
+						</form>
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<figure>
+							<img src="imagens/game.jpg" alt="Uncharted 4">
+							<figcaption>Uncharted 4</figcaption>
+						</figure>
+						<span> RS 100,00 </span>
+						<form action="">
+							<input type="submit" value="">
+						</form>
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<figure>
+							<img src="imagens/game.jpg" alt="Uncharted 4">
+							<figcaption>Uncharted 4</figcaption>
+						</figure>
+						<span> RS 100,00 </span>
+						<form action="">
+							<input type="submit" value="">
+						</form>
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<figure>
+							<img src="imagens/game.jpg" alt="Uncharted 4">
+							<figcaption>Uncharted 4</figcaption>
+						</figure>
+						<span> RS 100,00 </span>
+						<form action="">
+							<input type="submit" value="">
+						</form>
+					</a>
+				</li>	
+				<li>
+					<a href="#">
+						<figure>
+							<img src="imagens/game.jpg" alt="Uncharted 4">
+							<figcaption>Uncharted 4</figcaption>
+						</figure>
+						<span> RS 100,00 </span>
+						<form action="">
+							<input type="submit" value="">
+						</form>
+					</a>
+				</li>
+			</ul>
+		</section>
+		
+	</div>
+</div>	
---
+++ detalhes.php
@@ -0,0 +1,69 @@
+<div id="corpo_loja">
+	<aside class="banner">
+		<img src="imagens/banner.jpg" width="100%">
+	</aside>
+	<section class="categorias">
+		<h2 class="fundo_cinza"> Plataformas </h2>
+		<nav>
+			<ul>
+				<li><input type="checkbox" name="playstation"> PlayStation </li>
+				<li><input type="checkbox" name="xbox"> XBox </li>
+				<li><input type="checkbox" name="computador"> Computador </li>
+			</ul>
+		</nav>	
+		<h2 class="fundo_cinza"> Categorias </h2>
+		<nav>
+			<ul>
+				<li><input type="checkbox" name="acao"> Ação </li>
+				<li><input type="checkbox" name="aventura"> Aventura </li>
+				<li><input type="checkbox" name="sporte"> Esportes </li>
+			</ul>
+		</nav>	
+	</section>
+	<div id="lado_direito">
+		<section class="vitrine">
+			<dir id="cx_produto">
+				<a href="#"><img src="imagens/uncharted.jpg" alt="Uncharted 4" height="100%"></a>
+			</dir>
+			<div id="cx_titulo_produto">
+				<h1><a href="#"> Titulo do Jogo </a></h1>
+			</div>
+			<dir id="cx_preco_produto">
+				<span> Valor: </span><strong> R$ 100,00 </strong>
+			</dir>
+
+			<dir class="carac_plat_produto space_center">
+				<h3> Genero </h3>
+			</dir>
+			<dir class="carac_plat_produto">
+				<h4> Plataforma </h4>
+			</dir>
+			<div id="descricao_produto">
+				<h2> Descrição do jogo </h2>	
+				Esta é a sessão onde focará a descrição do jogo comtendo
+				caracteristicas, plataforma, pré-requisitos e etc...			
+			</div>
+			<dir id="comprar_produto">
+				<form action="">
+					<input type="submit" value="">
+				</form>
+			</dir>
+			<section id="abas-geral">
+				<ul class="menu">
+					<li><a href="#aba01"> Descrição </a></li>
+					<li><a href="#aba02"> Conteúdo </a></li>
+				</ul>
+				<section id="box">
+					<div id="aba01" class="conteudo">
+						<article id="descricao">
+							<h2> Descrição </h2>
+							<p>
+								Texto referente a descrição
+							</p>
+						</article>
+					</div>
+				</section>
+			</section>
+		</section>
+	</div>
+</dir>
