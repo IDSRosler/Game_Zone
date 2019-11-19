dbbe0ee336e47597abceb5a85714ece66d6381b2
IDSRosler <igorsouzarosler@gmail.com>
2019-11-19 12:57:16 -0300

    add: conexao com o banco de dados

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 		</section> <!--	fim div cabecalho -->
 
 		<section id="corpo">
-			<?php include_once("detalhes.php"); ?>
+			<?php include_once("home.php"); ?>
 		</section> <!--	fim div corpo -->
 
 		<footer id="rodape">
