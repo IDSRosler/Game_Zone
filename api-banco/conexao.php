555e9278b025d36a09bcb1b1b9c39dbeae10cd11
IDSRosler <igorsouzarosler@gmail.com>
2019-11-22 10:08:38 -0300

    add: api

--- api-banco/conexao.php
+++ api-banco/conexao.php
@@ -1,11 +1,10 @@
 <?php
-    
+    // Variaveis de controle
     $host     = 'localhost';
     $user     = 'root';
     $password = '';
     $database = 'gamezone';
-
+    // Conexão com o banco e base de dados 
     $link = mysqli_connect($host,$user,$password,$database) or die('Não foi possível conectar com o banco de dados');
     mysqli_select_db($link, $database) or die('Não foi possível conectar com a base de dados');
-
 ?>
